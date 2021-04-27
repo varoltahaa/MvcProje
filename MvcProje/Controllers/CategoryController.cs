7ac3f8eec599f027da591b891ce2c77e920e4070
varoltahaa <tahavarol98@hotmail.com>
2021-04-27 13:06:34 +0300

    27 Nisan 2021

--- MvcProje/Controllers/CategoryController.cs
+++ MvcProje/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,17 @@ namespace MvcProje.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager();
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAll();
+            return View(categoryValues);
+        }
+
+        
     }
 }
